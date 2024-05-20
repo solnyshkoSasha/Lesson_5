468370a38f24dce8db7f95f11e64f527276c7a67
solnyshkoSasha <158512299+solnyshkoSasha@users.noreply.github.com>
2024-05-20 12:48:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let num1 = 15;
+let num2 = 10;
+
+// let res = num1 - num2;
+console.log("Result: " + (num1 - num2));
+console.log("Result: " + (num1 + num2));
+console.log("Result: " + (num1 * num2));
+console.log("Result: " + (num1 / num2));
+console.log("Result: " + (num1 % num2));
+
+let num3 = 5;
+let num4 = "10";
+
+console.log(num3 + Number(num4));
+
+num1 *= 10;
+num1--;
+console.log(num1)
+
+let pi = Math.PI;
+console.log(Math.max(-1, 5, 2, 4));
